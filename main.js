4077052a04e0cce250e465054e835ce5470b7665
hazuki <pretty11_13@yahoo.co.jp>
2022-03-04 22:10:52 +0900

    わからない

--- main.js
+++ main.js
@@ -5,34 +5,6 @@ document.ready(function() {
 });
 
 
-$(function(){
-  $('.slider').slick({
-   
-  });
-});
-
-$(".bg_slide").bgswitcher({
-    effect: "fade",
-    interval: 2500,
-    images: ["images/background1.jpg",
-             "images/background2.jpg",
-             "images/background3.jpg",
-             "images/background4.jpg",
-             "images/background5.jpg"]
-  });
-
-
-jQuery(function($) {
-  $('.bg-slider').bgswitcher({
-  images: ['images/background1.jpg', 'images/background2.jpg', 'images/background3.jpg'], // 切り替える背景画像
-  interval: 5000, // 背景画像を切り替える間隔 1000=1秒
-  loop: true, // 切り替えを繰り返すか true=繰り返す　false=繰り返さない
-  shuffle: true, // 背景画像の順番をシャッフルするか true=する　false=しない
-  effect: "fade", // エフェクトの種類をfade,blind,clip,slide,drop,hideから指定
-  duration: 2000, // エフェクトの時間 1000=1秒
-  easing: "swing" // エフェクトのイージングをlinear,swingから指定
-  });
-  });
 
   particlesJS('js-particles',
   {
